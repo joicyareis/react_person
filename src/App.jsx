cabfbb5b0c043d59b943cba89f2f6389aacf38fd
joicyareis <jdcampchaves@gmail.com>
2026-04-09 20:48:13 +1000

    Task Solution review

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 
-import { Person } from './Components/Person/Person';
+import { Person } from './components/Person/Person';
 
 export const misha = {
   name: 'Misha',
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -7,7 +7,7 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
 
-      {age !== undefined && <p className="Person__age"> I am {age}</p>}
+      {age !== undefined && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">
         {isMarried
           ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
